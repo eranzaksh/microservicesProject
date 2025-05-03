e95a423bf1b105a3669df6df4f5ac36ed279b450
eran.zaksh <eranzaksh@gmail.com>
2025-05-03 09:21:48 +0300

    refigning ms2 with cron job

--- microservice2/app.py
+++ microservice2/app.py
@@ -13,34 +13,32 @@ s3 = boto3.client('s3', region_name=os.getenv('AWS_REGION'))
 SQS_QUEUE_URL = os.getenv('SQS_QUEUE_URL')
 S3_BUCKET_NAME = os.getenv('S3_BUCKET_NAME')
 
-# Read messages from queue. need to change out of while true.
+# Read messages from queue with cron job
 def process_messages():
-    while True:
-        response = sqs.receive_message(
-            QueueUrl=SQS_QUEUE_URL,
-            MaxNumberOfMessages=10,
-            WaitTimeSeconds=10  # long polling
+    response = sqs.receive_message(
+        QueueUrl=SQS_QUEUE_URL,
+        MaxNumberOfMessages=10,
+        WaitTimeSeconds=10  # long polling
+    )
+
+    messages = response.get('Messages', [])
+    for msg in messages:
+        body = msg['Body']
+        filename = f"inbox/{uuid.uuid4()}.json"
+
+        s3.put_object(
+            Bucket=S3_BUCKET_NAME,
+            Key=filename,
+            Body=body,
+            ContentType='application/json'
         )
 
-        messages = response.get('Messages', [])
-        for msg in messages:
-            body = msg['Body']
-            filename = f"inbox/{uuid.uuid4()}.json"
+        sqs.delete_message(
+            QueueUrl=SQS_QUEUE_URL,
+            ReceiptHandle=msg['ReceiptHandle']
+        )
+        print(f"Processed and uploaded: {filename}")
 
-            s3.put_object(
-                Bucket=S3_BUCKET_NAME,
-                Key=filename,
-                Body=body,
-                ContentType='application/json'
-            )
-
-            sqs.delete_message(
-                QueueUrl=SQS_QUEUE_URL,
-                ReceiptHandle=msg['ReceiptHandle']
-            )
-            print(f"Processed and uploaded: {filename}")
-
-        time.sleep(5)  # Pause before next polling
 
 if __name__ == "__main__":
     process_messages()
